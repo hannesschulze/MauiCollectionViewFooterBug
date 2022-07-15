01b3a9a9adc578db761b7c7352692ff4d9e6f9db
Hannes Schulze <haschu0103@gmail.com>
2022-07-15 15:28:31 +0200

    Simplify repro

--- MauiCollectionViewFooterBug/MainPage.xaml.cs
+++ MauiCollectionViewFooterBug/MainPage.xaml.cs
@@ -9,20 +9,16 @@ public sealed class ListItem
 
 public partial class MainPage : ContentPage
 {
-    public ObservableCollection<ListItem> Items { get; }
+    public ObservableCollection<ListItem> Items { get; } = new();
 
     public MainPage()
     {
-        Items = new ObservableCollection<ListItem> { new ListItem(), new ListItem() };
         BindingContext = this;
         InitializeComponent();
     }
 
-    private async void BreakFooter(object sender, EventArgs e)
+    private void AddItem(object sender, EventArgs e)
     {
-        Items.Clear();
-        await Task.Delay(TimeSpan.FromMilliseconds(250));
-        Items.Add(new ListItem());
         Items.Add(new ListItem());
     }
 }
