576a0a41a650217510ea5f30998ad97622e3dc47
Hannes Schulze <haschu0103@gmail.com>
2022-07-15 15:17:00 +0200

    Initial commit

---
+++ MauiCollectionViewFooterBug/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiCollectionViewFooterBug;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MauiCollectionViewFooterBug/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiCollectionViewFooterBug;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
